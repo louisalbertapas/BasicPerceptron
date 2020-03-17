19aa267e84a8342fec2b6531219f3ab328655ace
Louis Albert Apas <louisalbertapas25@gmail.com>
2020-03-17 19:42:54 +0800

    AND logic gate perceptron

---
+++ BasicPerceptron/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace BasicPerceptron
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTrain = new System.Windows.Forms.Button();
+            this.txtInput1 = new System.Windows.Forms.TextBox();
+            this.txtInput2 = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTrain
+            // 
+            this.btnTrain.Location = new System.Drawing.Point(12, 45);
+            this.btnTrain.Name = "btnTrain";
+            this.btnTrain.Size = new System.Drawing.Size(187, 75);
+            this.btnTrain.TabIndex = 0;
+            this.btnTrain.Text = "TRAIN";
+            this.btnTrain.UseVisualStyleBackColor = true;
+            this.btnTrain.Click += new System.EventHandler(this.btnTrain_Click);
+            // 
+            // txtInput1
+            // 
+            this.txtInput1.Enabled = false;
+            this.txtInput1.Location = new System.Drawing.Point(55, 123);
+            this.txtInput1.Name = "txtInput1";
+            this.txtInput1.Size = new System.Drawing.Size(42, 20);
+            this.txtInput1.TabIndex = 1;
+            // 
+            // txtInput2
+            // 
+            this.txtInput2.Enabled = false;
+            this.txtInput2.Location = new System.Drawing.Point(55, 149);
+            this.txtInput2.Name = "txtInput2";
+            this.txtInput2.Size = new System.Drawing.Size(42, 20);
+            this.txtInput2.TabIndex = 2;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Enabled = false;
+            this.btnTest.Location = new System.Drawing.Point(103, 123);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(96, 71);
+            this.btnTest.TabIndex = 3;
+            this.btnTest.Text = "TEST";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Enabled = false;
+            this.txtOutput.Location = new System.Drawing.Point(55, 174);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(42, 20);
+            this.txtOutput.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Input 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Input 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Output";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(36, 6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 36);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "A simple perceptron\r\nfor \"AND\" logic gate";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 205);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.txtInput2);
+            this.Controls.Add(this.txtInput1);
+            this.Controls.Add(this.btnTrain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Basic Perceptron";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTrain;
+        private System.Windows.Forms.TextBox txtInput1;
+        private System.Windows.Forms.TextBox txtInput2;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ BasicPerceptron/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BasicPerceptron
+{
+    public partial class Form1 : Form
+    {
+        double weight1, weight2;
+        double bias;
+        double learningRate;
+        int numberOfEpochs;
+        int[,] inputs;
+        int[] outputs;
+        Random rand = new Random();
+
+        private void btnTrain_Click(object sender, EventArgs e)
+        {
+            for (int epochItr = 0; epochItr < numberOfEpochs; epochItr++)
+            {
+                for(int inputItr = 0; inputItr < inputs.GetLength(0); inputItr++)
+                {
+                    double result_temp = inputs[inputItr, 0] * weight1 + inputs[inputItr, 1] * weight2 + bias;
+                    int result = result_temp > 0 ? 1 : 0;
+                    int delta = outputs[inputItr] - result;
+                    if (delta != 0)
+                    {
+                        weight1 += learningRate * delta * inputs[inputItr, 0];
+                        weight2 += learningRate * delta * inputs[inputItr, 1];
+                        bias += learningRate * delta;
+                    }
+                }
+            }
+            MessageBox.Show("Training finished");
+            txtInput1.Enabled = true;
+            txtInput2.Enabled = true;
+            btnTest.Enabled = true;
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int input1 = int.Parse(txtInput1.Text);
+                int input2 = int.Parse(txtInput2.Text);
+                double result_temp = input1 * weight1 + input2 * weight2 + bias;
+                int result = (result_temp > 0) ? 1 : 0;
+                txtOutput.Text = "" + result;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Please input only 1 or 0");
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            numberOfEpochs = 10;
+            learningRate = 0.1;
+
+            inputs = new int[,]
+            {
+                {0,0 },
+                {0,1 },
+                {1,0 },
+                {1,1 }
+            };
+
+            outputs = new int[]
+            {
+                0,
+                0,
+                0,
+                1
+            };
+
+            weight1 = rand.NextDouble();
+            weight2 = rand.NextDouble();
+            bias = rand.NextDouble();
+        }
+    }
+}
